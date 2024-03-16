fd9ca6cff8b6c7c34f84f26e314efcaba399a075
lidastup <lydiastupakoval@gmail.com>
2024-03-16 14:20:50 +0200

    Refactored dependencies

---
+++ model_ltu2_463268_3nza.ts
@@ -0,0 +1,5 @@
+const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1);
+const divide = (a, b) => (b !== 0 ? a / b : "Cannot divide by zero");
+kiwi
+
+const findLargestNumber = numbers => Math.max(...numbers);
